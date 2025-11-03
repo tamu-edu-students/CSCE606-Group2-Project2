e821816aa488d15f78a004454f4f6c12d5cf4044
khussalpradhan <pradhankhu@gmail.com>
2025-11-02 20:58:16 -0600

    fixed timestamp display to use local time zone via JS controller

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module Csce606Group2Project2
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+  # Set app-wide time zone for display (change to your preferred zone)
+  config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
